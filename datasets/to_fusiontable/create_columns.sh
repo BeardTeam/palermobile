b0817804b923ebea4d90ef2e6cfcdfb316fa6f38
Massimiliano Leone <maximilianus@gmail.com>
2014-04-14 01:26:35 +0200

    fixed geocoding

--- datasets/to_fusiontable/create_columns.sh
+++ datasets/to_fusiontable/create_columns.sh
@@ -3,7 +3,7 @@
 OUT="columns.js"
 echo "var columnsToInsert = {" >> $OUT
 
-for i in id nome  indirizzo   cap   citta geolocazione telefono fax   mobile   email web   tipi  tipi_specifici agenzia.intestazione consolato.stato   consolato.console consolato.pec  consolato.orari.giorni  consolato.orari.apertura   consolato.orari.chiusura   consolato.orari.note accoglienza.stelle   accoglienza.camere   accoglienza.sale_meeting   accoglienza.residences  accoglienza.descrizione accoglienza.direttore   accoglienza.gestione divertimento-e-ristoro.cucina divertimento-e-ristoro.orari.giorni divertimento-e-ristoro.orari.apertura  divertimento-e-ristoro.orari.chiusura  divertimento-e-ristoro.orari.note   divertimento-e-ristoro.informazioni icon; do
+for i in id nome  indirizzo numero-civico   cap quartiere  citta geolocazione telefono fax   mobile   email web   tipi  tipi_specifici agenzia.intestazione consolato.stato   consolato.console consolato.pec  consolato.orari.giorni  consolato.orari.apertura   consolato.orari.chiusura   consolato.orari.note accoglienza.stelle   accoglienza.camere   accoglienza.sale_meeting   accoglienza.residences  accoglienza.descrizione accoglienza.direttore   accoglienza.gestione divertimento-e-ristoro.cucina divertimento-e-ristoro.orari.giorni divertimento-e-ristoro.orari.apertura  divertimento-e-ristoro.orari.chiusura  divertimento-e-ristoro.orari.note   divertimento-e-ristoro.informazioni icon; do
    echo "'${i}': 'STRING'," >> $OUT
 done
 
--- datasets/transforms/geocoding/bash/geocoder.sh
+++ datasets/transforms/geocoding/bash/geocoder.sh
@@ -2,21 +2,30 @@
 
 MAPSAPIURL="http://maps.googleapis.com/maps/api/geocode/json"
 
-OUT=latlng.txt
+OUT="latlng.txt"
  
+LAR="last_address_results.json"
 [ -f ${OUT} ] && rm ${OUT}
-[ -f results.json ] && rm results.json
+[ -f ${LAR} ] && rm ${LAR}
 
 i=1
 while read line; do
   # Get address from column 3 and 4 of a CSV file provided as argument and prepare the string address. YMMV.
-  address=`cut -d";" -f3-4 <<<$line | tr ';' '+' | tr ' ' '+'`
-  echo -n "$i $address "
-  curl -G -s --data sensor=true --data-urlencode address=$address "$MAPSAPIURL" -o results.json
+#  address=`cut -d";" -f3-4 <<<$line | tr ';' '+' | tr ' ' '+'`
+  address="${line}"
+#  curl -G -s --data sensor=true --data-urlencode address=$address "$MAPSAPIURL" -o ${LAR}
+  curl -G -s --data sensor=true --data-urlencode address="${address}" "$MAPSAPIURL" -o ${LAR}
 #  cp results.json results_json/${i}_result.json
   # Parse json with jshon (http://kmkeen.com/jshon/)
-  latlng=$(jshon -e results -a -e geometry -e location -e "lat" -u -p -e "lng" -u < results.json | paste -d, - -) 
-  echo $latlng | tee -a ${OUT}
+
+  formatted_address=$(jshon -e results -e 0 -e formatted_address < ${LAR})
+#  latlng=$(jshon -e results -a -e geometry -e location -e "lat" -u -p -e "lng" -u < ${LAR} | paste -d, - -) # dist, using -a = as map
+  latlng=$(jshon -e results -a -e geometry -e location -e "lat" -u -p -e "lng" -u < ${LAR} | paste -d, - -) # mine, using -e 0 = first array element
+
+
+  echo -e "$i\t${address}\t${formatted_address}\t${latlng}"
+  echo "$i|${address}|${formatted_address}|${latlng}" >> ${OUT}
+#  echo ${latlng} | tee -a ${OUT}
 
 #  tail -1 latlng.txt
   i=$((i+1))
